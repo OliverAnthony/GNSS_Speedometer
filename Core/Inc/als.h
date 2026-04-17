cb2cb825c44b5dbc82008fa72a1921a18e758832
Oliver Anthony <yixiang.gg.10@gmail.com>
2026-04-17 22:28:25 +0800

    feat: ALS implementation

---
+++ Core/Inc/als.h
@@ -0,0 +1,18 @@
+/* LTR-308ALS */
+#ifndef ALS_H
+#define ALS_H
+
+#include "main.h"
+
+#define ALS_ADDR 0xA6
+#define ALS_MAIN_CTRL 0x00
+#define ALS_MEAS_RATE 0x04
+#define ALS_GAIN 0x05
+#define ALS_DATA_0 0x0D
+#define ALS_DATA_1 0x0E
+#define ALS_DATA_2 0x0F
+
+void ALS_Init(void);
+void ALS_Read(const uint8_t reg, uint8_t *data, const uint16_t size);
+
+#endif /* ALS_H */
